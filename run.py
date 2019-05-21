37d338190d1865fbd8620dc402039cd3a915ddd2
LukaszPlawinski <plawinski.lukasz@gmail.com>
2019-05-21 08:13:50 +0000

    created contact.html and use of Jinja template logic

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
     
-@app.route("/about")
-def about():
-    return "About page"
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
     
 if __name__=='__main__':
     app.run(host=os.environ.get("IP"),
