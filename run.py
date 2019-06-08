08293af6389445ef6870d2f46cbe795c18343fe1
LukaszPlawinski <plawinski.lukasz@gmail.com>
2019-06-08 09:28:15 +0000

    created add_recipe file and add_recipe route

--- run.py
+++ run.py
@@ -22,13 +22,28 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
-
-
+    
+@app.route('/add_recipe')
+def add_recipe():
+   return render_template("add_recipe.html", 
+   recipe =mongo.db.recipes.find())
+   
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+    
+    
 @app.route('/<recipe_id>')
 def about_recipe(recipe_id):
     return render_template('meal.html',
     recipe=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
     
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db. recipes.find_one({'_id': ObjectId(recipe_id)})
+    return render_template('edit_recipe.html', recipe= the_recipe)
 
 @app.route("/recipes")
 def recipes():
