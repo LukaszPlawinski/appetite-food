b09aa6d0a1b680cf9303890274338b165a94be14
Ubuntu <ubuntu@ip-172-31-92-13.ec2.internal>
2019-06-28 10:19:16 +0000

    added modal popup with alerts after login

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
  $(document).ready(function() {
- 
- 
- $(document).on("click", "a.remove_ingredient, a.remove_step" , function() {
-            $(this).closest(".row").remove();
-        });
-        $(document).on("click","#add_ingredient",function(){
-            var new_ingredient =` <div class="row" >
+
+
+     $(document).on("click", "a.remove_ingredient, a.remove_step", function() {
+         $(this).closest(".row").remove();
+     });
+     $(document).on("click", "#add_ingredient", function() {
+         var new_ingredient = ` <div class="row" >
                         <div class="name col-10">
                             <div class="control-group">
                                 <div class="form-group floating-label-form-group controls">
@@ -20,10 +20,10 @@
                             <a href="javascript:void(0);" class="remove_ingredient"><i class="fas fa-trash-alt red"></i></a>
                         </div>
                     </div>`;
-            $(new_ingredient).appendTo('#ingredients');
-        });
-         $(document).on("click","#add_step",function(){
-            var new_step = ` <div class="row ">
+         $(new_ingredient).appendTo('#ingredients');
+     });
+     $(document).on("click", "#add_step", function() {
+         var new_step = ` <div class="row ">
                         <div class="control-group col-10">
                             <div class="form-group floating-label-form-group controls">
                                 <label>Step</label>
@@ -35,9 +35,16 @@
                             <a href="javascript:void(0);" class="remove_step"><i class="fas fa-trash-alt delete red"></i></a>
                         </div>
                     </div>`
-            $(new_step).appendTo("#steps");
-        });
-        $(function() {
-            $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
-        });
-})
+         $(new_step).appendTo("#steps");
+     });
+     $(function() {
+         $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+     });
+     $('#alert_modal').modal('show');
+     setTimeout(function() {
+         $('#alert_modal').modal('hide');
+     }, 2000);
+
+
+ })
+ 
